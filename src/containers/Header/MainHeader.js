86884b99d40a500b6bac770423f52fa8e863c035
nisoni <nidhi.soni@medlife.com>
2020-07-27 17:10:14 +0530

    changes in view

--- src/containers/Header/MainHeader.js
+++ src/containers/Header/MainHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {logo} from "../../static/svgs"
+import logo from "../../static/images/wall-e_big.png"
 import "./style.scss"
 
 export default function MainHeader ({net_state }) {
 
   return (<header className={`row header-container ${!net_state ? "grey-out" : ""}`}>
     <section className="row logo-wraper">
-      {logo()} 
+      <img className="logo" src={logo}/>
     </section>
     <section className="row"><span className="rupee-logo">₹</span></section>
   </header>)
